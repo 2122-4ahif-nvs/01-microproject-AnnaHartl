a6ecfc88cf1d197850cabc7b1344697033f9c391
AnnaHartl <anna.sofie.hartl@gmail.com>
2021-12-26 09:41:12 +0100

    added entity Invoice

---
+++ backend/src/main/java/at/htl/entity/Invoice.java
@@ -0,0 +1,21 @@
+package at.htl.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Invoice extends PanacheEntityBase {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @ManyToOne
+    public Customer customer;
+    @ManyToOne
+    public Employee employee;
+
+    public LocalDate purchaseDate;
+}
